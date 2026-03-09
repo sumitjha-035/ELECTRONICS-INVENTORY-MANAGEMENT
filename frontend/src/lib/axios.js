84afdb23561d4068ceace7680851a46cedb09b50
sumitjha-035 <chmcs2026.sumit.jha@gmail.com>
2026-03-09 17:12:33 +0530

    Change API base URL to production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://electronics-inventory-management.onrender.com'
 });
 export default api;
